e3feb6c87de63fc6977bc8135dc2ef78ff3e3c04
Ningyuan Li <mariotaku.lee@gmail.com>
2023-06-14 00:19:06 +0900

    added evil hack to fix curl abi version problem

---
+++ platform/webos/curl-abi-fix/lib.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+
+__attribute__((unused)) static void init() __attribute__((constructor));
+
+__attribute__((unused)) void init() {
+}
---
+++ platform/webos/curl-abi-fix/libcurl.so.5/lib.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+__attribute__((unused)) static void init() __attribute__((constructor));
+
+__attribute__((unused)) void init() {
+    abort();
+}
