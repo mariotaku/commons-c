111312870c3ac05bffa1b672942dc9b771111aee
Ningyuan Li <mariotaku.lee@gmail.com>
2023-06-20 19:25:59 +0900

    fixing build for commons

--- platform/webos/panel-info/panel_info.c
+++ platform/webos/panel-info/panel_info.c
@@ -2,9 +2,9 @@
 
 #include <SDL_types.h>
 
-SDL_bool SDL_webOSGetPanelResolution(int *width, int *height) __attribute__((weak_import));
+SDL_bool SDL_webOSGetPanelResolution(int *width, int *height) __attribute__((weak));
 
-SDL_bool SDL_webOSGetRefreshRate(int *rate) __attribute__((weak_import));
+SDL_bool SDL_webOSGetRefreshRate(int *rate) __attribute__((weak));
 
 int commons_webos_get_panel_info(webos_panel_info_t *info) {
     if (SDL_webOSGetPanelResolution == NULL || SDL_webOSGetRefreshRate == NULL) {
