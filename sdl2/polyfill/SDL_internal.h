c69b1468e1f8fd1ceba67cae16a2f959dd2a7b3c
Ningyuan Li <mariotaku.lee@gmail.com>
2023-07-24 19:49:22 +0900

    sdl2 polyfill

---
+++ sdl2/polyfill/SDL_internal.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "SDL_polyfill_target.h"
+
+#define POLYFILL __attribute__((unused))
+#define NEED_POLYFILL(major, minor, patch) (COMMONS_SDL2_POLYFILL_TARGET_MINOR < (minor) || COMMONS_SDL2_POLYFILL_TARGET_PATCH < (patch))
