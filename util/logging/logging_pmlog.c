241c78c0bf74962da0f7aa5256821843773ee564
Ningyuan Li <mariotaku.lee@gmail.com>
2023-09-20 21:03:50 +0900

    fixed build

--- util/logging/logging_pmlog.c
+++ util/logging/logging_pmlog.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-
 #include "logging.h"
 
-#include <SDL.h>
+#include <stdio.h>
+#include <string.h>
 #include <PmLogLib.h>
 
-static SDL_mutex *mutex = NULL;
 static PmLogContext context;
+static struct timespec ts_init;
 
 void commons_logging_init(const char *context_name) {
     PmLogGetContext(context_name != NULL ? context_name : "commons_logging", &context);
+    clock_gettime(CLOCK_MONOTONIC, &ts_init);
 }
 
 void commons_logging_deinit() {
@@ -24,26 +24,29 @@ void commons_log_vprintf(commons_log_level level, const char *tag, const char *f
         *brk = '\0';
     }
     FILE *output = stdout;
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    float timestamp = (float) (ts.tv_sec - ts_init.tv_sec) + (float) (ts.tv_nsec - ts_init.tv_nsec) / 1000000000.0f;
     switch (level) {
         case COMMONS_LOG_LEVEL_FATAL:
             output = stderr;
-            PmLogCritical(context, tag, 0, "[%.03f] %s", ((float) SDL_GetTicks() / 1000.0f), msg);
+            PmLogCritical(context, tag, 0, "[%.03f] %s", timestamp, msg);
             break;
         case COMMONS_LOG_LEVEL_ERROR:
             output = stderr;
-            PmLogError(context, tag, 0, "[%.03f] %s", ((float) SDL_GetTicks() / 1000.0f), msg);
+            PmLogError(context, tag, 0, "[%.03f] %s", timestamp, msg);
             break;
         case COMMONS_LOG_LEVEL_WARN:
             output = stderr;
-            PmLogWarning(context, tag, 0, "[%.03f] %s", ((float) SDL_GetTicks() / 1000.0f), msg);
+            PmLogWarning(context, tag, 0, "[%.03f] %s", timestamp, msg);
             break;
         case COMMONS_LOG_LEVEL_INFO:
-            PmLogInfo(context, tag, 0, "[%.03f] %s", ((float) SDL_GetTicks() / 1000.0f), msg);
+            PmLogInfo(context, tag, 0, "[%.03f] %s", timestamp, msg);
             break;
         case COMMONS_LOG_LEVEL_DEBUG:
         case COMMONS_LOG_LEVEL_VERBOSE:
-            PmLogDebug(context, "[%.03f] %s", ((float) SDL_GetTicks() / 1000.0f), msg);
+            PmLogDebug(context, "[%.03f] %s", timestamp, msg);
             break;
     }
-    fprintf(output, "[%.03f][%s] %s\n", ((float) SDL_GetTicks() / 1000.0f), tag, msg);
+    fprintf(output, "[%.03f][%s] %s\n", timestamp, tag, msg);
 }
