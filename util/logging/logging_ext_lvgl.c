316cb23fe77632f9121bfc5d75fdd1c1b79f05e5
Ningyuan Li <mariotaku.lee@gmail.com>
2024-03-18 15:28:15 +0900

    skips line break in lvgl logging

--- util/logging/logging_ext_lvgl.c
+++ util/logging/logging_ext_lvgl.c
@@ -10,7 +10,7 @@ void commons_lv_log(const char *message) {
         const char *start = cur;
         cur = strchr(cur, '\n');
         if (cur != NULL) {
-            int line_len = cur - start;
+            size_t line_len = cur - start;
             if (line_len <= 0) {
                 break;
             }
@@ -24,6 +24,7 @@ void commons_lv_log(const char *message) {
 
 static void app_lv_log_line(const char *line, size_t len) {
     const char *start = memchr(line, '\t', len) + 1;
+    size_t msg_len = len - (start - line);
     static const commons_log_level level_value[] = {
             COMMONS_LOG_LEVEL_VERBOSE, COMMONS_LOG_LEVEL_INFO, COMMONS_LOG_LEVEL_WARN, COMMONS_LOG_LEVEL_ERROR, COMMONS_LOG_LEVEL_DEBUG,
     };
@@ -32,7 +33,7 @@ static void app_lv_log_line(const char *line, size_t len) {
     };
     for (int i = 0; i < sizeof(level_value) / sizeof(commons_log_level); i++) {
         if (strncmp(level_name[i], line + 1, (start - line - 3)) == 0) {
-            commons_log_printf(level_value[i], "LVGL", "%s", start);
+            commons_log_printf(level_value[i], "LVGL", "%.*s", (int) msg_len, start);
             return;
         }
     }
