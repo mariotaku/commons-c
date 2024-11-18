c8404ce85742ea125bbfd3878b6997b8c27ca958
Ningyuan Li <mariotaku.lee@gmail.com>
2024-11-18 18:20:30 +0900

    added static inih

--- cmake/ExternalINIH.cmake
+++ cmake/ExternalINIH.cmake
@@ -7,14 +7,18 @@ if (NOT TARGET ext_inih)
     include(ExternalProject)
 
     set(EXT_INIH_CMAKELISTS_FILE "${CMAKE_CURRENT_BINARY_DIR}/ext_inih_cmake.txt")
-    file(WRITE "${EXT_INIH_CMAKELISTS_FILE}" [=[
+    set(EXT_INIH_LIBRARY_TYPE SHARED)
+    if (INIH_STATIC)
+        set(EXT_INIH_LIBRARY_TYPE STATIC)
+    endif ()
+    file(WRITE "${EXT_INIH_CMAKELISTS_FILE}" "
 cmake_minimum_required(VERSION 3.0)
 project(inih LANGUAGES C)
-add_library(inih SHARED ini.c)
+add_library(inih ${EXT_INIH_LIBRARY_TYPE} ini.c)
 set_target_properties(inih PROPERTIES SOVERSION 1)
-install(FILES "ini.h" TYPE INCLUDE)
-install(TARGETS inih LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} NAMELINK_SKIP)
-]=])
+install(FILES \"ini.h\" TYPE INCLUDE)
+install(TARGETS inih LIBRARY DESTINATION \${CMAKE_INSTALL_LIBDIR} NAMELINK_SKIP)
+")
 
     set(EXT_INIH_TOOLCHAIN_ARGS)
     if (CMAKE_TOOLCHAIN_FILE)
@@ -24,32 +28,38 @@ install(TARGETS inih LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} NAMELINK_SKIP)
         list(APPEND EXT_INIH_TOOLCHAIN_ARGS "-DCMAKE_TOOLCHAIN_ARGS:string=${CMAKE_TOOLCHAIN_ARGS}")
     endif ()
 
-    if (CMAKE_SHARED_LIBRARY_SUFFIX STREQUAL ".so")
+    if (EXT_INIH_LIBRARY_TYPE STREQUAL "STATIC")
+        set(LIBRARY_NAME "${CMAKE_STATIC_LIBRARY_PREFIX}inih${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    elseif (CMAKE_SHARED_LIBRARY_SUFFIX STREQUAL ".so")
         set(LIBRARY_NAME "${CMAKE_SHARED_LIBRARY_PREFIX}inih.so.1")
     else ()
         set(LIBRARY_NAME "${CMAKE_SHARED_LIBRARY_PREFIX}inih${CMAKE_SHARED_LIBRARY_SUFFIX}")
     endif ()
 
     ExternalProject_Add(ext_inih
-            URL https://github.com/benhoyt/inih/archive/refs/tags/r56.tar.gz
+            URL https://github.com/benhoyt/inih/archive/refs/tags/r58.tar.gz
             PATCH_COMMAND cmake -E copy "${EXT_INIH_CMAKELISTS_FILE}" CMakeLists.txt
             CMAKE_ARGS ${EXT_INIH_TOOLCHAIN_ARGS}
             -DCMAKE_BUILD_TYPE:string=${CMAKE_BUILD_TYPE}
             -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
             BUILD_BYPRODUCTS <INSTALL_DIR>/lib/${LIBRARY_NAME}
-            )
+    )
     ExternalProject_Get_Property(ext_inih INSTALL_DIR)
 
-    add_library(ext_inih_target SHARED IMPORTED)
-    set_target_properties(ext_inih_target PROPERTIES IMPORTED_LOCATION ${INSTALL_DIR}/lib/${LIBRARY_NAME})
-
+    add_library(ext_inih_target ${EXT_INIH_LIBRARY_TYPE} IMPORTED)
     add_dependencies(ext_inih_target ext_inih)
 
-    if (NOT DEFINED CMAKE_INSTALL_LIBDIR)
-        set(CMAKE_INSTALL_LIBDIR lib)
+    message(${INSTALL_DIR}/lib/${LIBRARY_NAME})
+    set_target_properties(ext_inih_target PROPERTIES IMPORTED_LOCATION ${INSTALL_DIR}/lib/${LIBRARY_NAME})
+
+    if (EXT_INIH_LIBRARY_TYPE STREQUAL "SHARED")
+        if (NOT DEFINED CMAKE_INSTALL_LIBDIR)
+            set(CMAKE_INSTALL_LIBDIR lib)
+        endif ()
+
+        install(DIRECTORY ${INSTALL_DIR}/lib/ DESTINATION ${CMAKE_INSTALL_LIBDIR})
     endif ()
 
-    install(DIRECTORY ${INSTALL_DIR}/lib/ DESTINATION ${CMAKE_INSTALL_LIBDIR})
 endif ()
 
 set(INIH_INCLUDE_DIRS ${INSTALL_DIR}/include)
